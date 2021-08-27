433c6d16ef448b394e21e836323b1bbe34a6b5bb
zlaxton <zlatan.kopic171@yahoo.com>
2021-08-27 16:05:37 +0200

    update

--- index.js
+++ index.js
@@ -1,133 +1,39 @@
 const express = require("express"),
-  const morgan = require("morgan");
+  morgan = require("morgan");
 (bodyParser = require("body-parser")), (uuid = require("uuid"));
-
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
+const mongoose = require("mongoose");
+const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
+const Directors = Models.Directors;
+const Genres = Models.Genres;
 
-mongoose.connect('mongodb://localhost:27017//myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
+//Log requests to server
 app.use(morgan("common"));
 
-app.use("/public", express.static("public"));
+app.use("/documentation", express.static("public"));
 
-let movies = [
-  {
-    id: "1",
-    title: "Silence of the Lambs",
-    year: "2016",
-    director: "Jonathan Demme",
-    genres: "Thriller",
-    description:
-      "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-  },
-  {
-    id: "2",
-    title: "The Hobbit: The Battle of the Five Armies",
-    year: "2014",
-    director: "Peter Jackson",
-    genres: "Adventure, Fantasy",
-    description:
-      "The Battle of Five Armies was a battle waged between the Orcs and the Wargs of the Misty Mountains and the Grey Mountains against the Lake-men, Elves, Dwarves, on and near the Lonely Mountain.",
-  },
-
-  {
-    id: "3",
-    title: "Indiana Jones and the Kingdom of the Crystal Skull",
-    year: "2008",
-    director: "Steven Spielberg",
-    genres: "Adventure",
-    description:
-      "Released and taking place nineteen years after the previous film, it is set in 1957, pitting Indiana Jones (Harrison Ford) against Soviet agents led by Irina Spalko (Cate Blanchett)—searching for a telepathic crystal skull. ... The filmmakers intended to pay tribute to the science fiction B-movies of the 1950s era.",
-  },
-
-  {
-    id: "4",
-    title: "Minority Report",
-    year: "2002",
-    director: "Steven Spielberg",
-    genres: "Mystery",
-    description:
-      "In a future where a special police unit is able to arrest murderers before they commit their crimes, an officer from that unit is himself accused of a future murder.",
-  },
-  {
-    id: "5",
-    title: "The Lord of the Rings: The Return of the King",
-    year: "2003",
-    director: "Peter Jackson",
-    genres: "Adventure, Fantasy",
-    description:
-      "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
-  },
-
-  {
-    id: "6",
-    title: "The Terminal",
-    year: "2004",
-    director: "Steven Spielberg",
-    genres: "Drama",
-    description:
-      "An Eastern European tourist unexpectedly finds himself stranded in JFK airport, and must take up temporary residence there.",
-  },
-
-  {
-    id: "7",
-    title: "War of the Worlds",
-    year: "2005",
-    director: "‎Steven Spielberg",
-    genres: "Science fiction",
-    description:
-      "Sci-fi action film about an alien invasion threatening the future of humanity. The catastrophic nightmare is depicted through the eyes of one American family fighting for survival.",
-  },
-  {
-    id: "8",
-    title: "Divergent",
-    year: "2014",
-    director: "Neil Burger",
-    genres: "Science fiction",
-    description:
-      "In a world divided by factions based on virtues, Tris learns she's Divergent and won't fit in. When she discovers a plot to destroy Divergents, Tris and the mysterious Four must find out what makes Divergents dangerous before it's too late.",
-  },
-
-  {
-    id: "9",
-    title: "Shutter Island",
-    year: "2010",
-    director: "Martin Scorsese",
-    genres: "Thriller",
-    description:
-      "In 1954, a U.S. Marshal investigates the disappearance of a murderer who escaped from a hospital for the criminally insane.",
-  },
-
-  {
-    id: "10",
-    title: "The Godfather",
-    year: "1972",
-    director: "Francis Ford Coppola",
-  },
-  {
-    id: "11",
-    title: "Blood In Blood Out",
-    year: "1993",
-    director: "Taylor Hackford",
-  },
-];
+mongoose.connect("mongodb://localhost:27017//myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", (req, res) => {
-  res.status(200).sendFile(`${__dirname}/public/index.html`);
+  res.send("Welcome to myFlixDB!");
 });
 
 // Gets the list of data about ALL movies
-app.get("/movies", (req, res) => {
-  res.json(movies);
+app.get("/movies", async (req, res) => {
+  try {
+    const movies = await Movies.find();
+    res.status(201).json(movies);
+  } catch (err) {
+    res.json({ message: "Movies could not be accessed" });
+  }
 });
 
 app.get("/", (req, res) => {
@@ -142,8 +48,31 @@ app.get("/movies/:title", (req, res) => {
     })
   );
 });
+
 app.get("/documentation", (req, res) => {
   res.status(200).sendFile(`${__dirname}/public/documentation.html`);
+});
+
+//Route to Data about Director  **************************************
+app.get("/directors/:Name", async (req, res) => {
+  try {
+    const director = await Directors.findOne({ Name: req.params.Name });
+    res.json(director);
+  } catch (err) {
+    res.json({ message: "Info about director could not be accessed" });
+  }
+});
+
+//Route to Data about Genre  ****************************************
+app.get("/genres/:genre", (req, res) => {
+  Genres.find({ Genre: req.body.genre })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Add a movie
@@ -180,49 +109,133 @@ app.put("/movies/:title/:year", (req, res) => {
 });
 
 // Creating a new user
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
+        return res.status(400).send(req.body.Username + "already exists");
       } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
         })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
       }
     })
     .catch((error) => {
       console.error(error);
-      res.status(500).send('Error: ' + error);
+      res.status(500).send("Error: " + error);
+    });
+});
+// Get all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
 });
 
-// Deletes a movie from our list by id
-app.delete("/movies/:id", (req, res) => {
-  let movie = movies.find((movie) => {
-    return movie.id === req.params.id;
-  });
-
-  if (movie) {
-    movies = movies.filter((obj) => {
-      return obj.id !== req.params.id;
+// Get a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
-    res
-      .status(201)
-      .send("Movie with the ID of " + req.params.id + " was deleted.");
-  } else {
-    res.status(404).send(`Movie with the id ${req.params.id} was not found.`);
-  }
 });
+// Update users with a certain name
+app.put("/users/:Username", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+// Add a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+// Delete a user by username
+app.delete("/users/:Username", (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
+//Route for users to remove movies from favorite list
+app.delete("/users/:Username/movies/delete/:MovieID", (req, res) => {
+  Users.findOneAndUpdate(
+    { username: req.params.Username },
+    {
+      $pull: { favoriteMovie: req.params.MovieID },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
 // Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
