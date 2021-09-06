38b65da024a2a06a659a7c4d592c8479fdbdf484
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-06 11:23:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-/*mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});*/
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
