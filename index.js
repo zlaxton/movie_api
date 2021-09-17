f34a405a5e15b4c53227a1d7bad1ba19c59da94c
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-17 12:18:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,23 @@ const app = express();
 const cors = require("cors");
 
 const { check, validationResult } = require("express-validator");
-
+let allowedOrigins = ["http://localhost:1234"];
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
