1ac75e4cd254836a9885c9b78ad7e9c82bc4bc67
zlaxton <zlatan.kopic171@yahoo.com>
2021-09-17 12:01:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,20 +21,7 @@ let allowedOrigins = [
 ];
 
 //Middleware
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message =
-          "The CORS policy for this application does not allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
