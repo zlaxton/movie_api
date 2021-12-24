92d65587099e8231b47f6eb77066257b3f23b288
zlaxton <zlatan.kopic171@yahoo.com>
2021-12-24 19:02:05 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -13,7 +13,11 @@ let generateJWTToken = (user) => {
   });
 };
 
-/* POST login. */
+/**
+ * POST login.
+ * @param {*} router
+ * @returns {object} Token, User
+ */
 module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user, info) => {
