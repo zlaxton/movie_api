ebcb844b3ce39126f1b37d1147d73045d07f8070
zlaxton <zlatan.kopic171@yahoo.com>
2022-01-05 20:07:58 +0100

    index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(morgan("common"));
-app.use("/documentation", static("public"));
+//app.use("/documentation", static("public"));
 
 const auth = require("./auth.js")(app);
 
